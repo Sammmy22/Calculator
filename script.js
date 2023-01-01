23341c7e5baa79e9bebd75fb10290dfabd100811
Sammmy22 <sambhavc225@outlook.com>
2023-01-01 21:42:45 +0530

    Now calculating works!!!

--- script.js
+++ script.js
@@ -21,26 +21,51 @@ let operation = document.querySelector(".operation");
 
 document.querySelector("#clear").addEventListener("click", function () {
   result.innerHTML = "";
-  operation.innerHTML = "";
+  sign.innerHTML = "";
+  prev.innerHTML = "";
 });
-
-let n1;
-let n2;
-let answer;
 
 const num_btns = document.querySelectorAll(".nmbr");
 
 // console.log(num_btns);
 
-function parse(a) {
-  let arr = [];
-  arr.push(a);
-}
-
 for (let i = 0; i < num_btns.length; i++) {
   num_btns[i].addEventListener("click", () => {
     let result = document.querySelector(".result");
     // console.log(num_btns[i].innerHTML);
-    result.innerHTML = num_btns[i].innerHTML;
+    result.innerHTML += num_btns[i].innerHTML;
+    // result.innerHTML = parse(num_btns[i].innerHTML);
   });
 }
+
+const backspace = document.querySelector("#back");
+
+backspace.addEventListener("click", () => {
+  result.innerHTML = result.innerHTML.slice(0, result.innerHTML.length - 1);
+});
+
+const operators = document.querySelectorAll(".ops");
+
+console.log(operators);
+
+const sign = document.querySelector("#sign");
+const prev = document.querySelector("#prev");
+
+for (let i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", () => {
+    prev.innerHTML = result.innerHTML;
+    sign.innerHTML = operators[i].innerHTML;
+    result.innerHTML = "";
+  });
+}
+
+document.querySelector("#eval").addEventListener("click", () => {
+  result.innerHTML = Calculate(
+    prev.innerHTML,
+    result.innerHTML,
+    sign.innerHTML
+  );
+
+  prev.innerHTML = "";
+  sign.innerHTML = "";
+});
